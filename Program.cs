07c4f9258e621aaa6225c73c0211b93a410c8c21
Anatoly Tuzovsky <taf@artcollege.ru>
2024-05-10 15:40:40 +0300

    Добавлены операторы ввода данных

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Введите X = ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
     }
 }
